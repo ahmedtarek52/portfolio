3b7d1f9651fdf4d0b28b884ff048cad018c7dd40
ahmed <ahmed115@gmail.com>
2024-05-01 23:32:53 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const allTaps = document.querySelectorAll('.tap-contants');
+const allLinks = document.querySelectorAll('.tap-links');
+function opentap(tapName) {
+    allTaps.forEach(tap => tap.classList.remove('active-tap'));
+    allLinks.forEach(link => link.classList.remove('active-link'));
+    document.getElementById(tapName).classList.add('active-tap');
+    event.currentTarget.classList.add('active-link');
+  }
+
+
+  var sidemenu = document.getElementById('sidemenu')
+  function openMenu(){
+    sidemenu.style.right= "0"
+  }
+  function closeMenu(){
+    sidemenu.style.right= "-150px"
+  }
+
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyT_eEt5-eFrWbukO0e9pbnCJyAOVMScqRaofKDvKAVimyFIzjrk3EisAAGEDyRtKoL/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById( 'msg' )
+  form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+          .then(response => {
+            msg.innerHTML="Your message sent successfully";
+            setTimeout(function(){
+              msg.innerHTML= ' ';
+            },5000)
+            form.reset()
+  })
+          .catch(error => console.error('Error!', error.message))
+  })
